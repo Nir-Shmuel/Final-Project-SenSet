3c135a0fe488bbea3066da7c1cc7fc6757686d94
Lior Gingihashvili <45664120+LiorGingi@users.noreply.github.com>
2020-05-13 01:46:22 +0300

    Fix issues with CNN Network and adjustments to research

--- CNN_net.py
+++ CNN_net.py
@@ -9,43 +9,37 @@ class CnnModel(Sequential):
         super().__init__()
         self.add(Input(shape=(None, pixels_x, pixels_y, channels), name='input'))
         self.add(
-            layers.Conv3D(32, (3, 3, 3), strides=(2, 2, 2), padding='same', use_bias=False,
-                          kernel_initializer='glorot_normal', kernel_regularizer=regularizers.l2))
+            layers.Conv3D(filters=32, kernel_size=(3, 3, 3), padding='same', use_bias=False,
+                          kernel_initializer='glorot_normal'))
         self.add(layers.LeakyReLU(alpha=0.3))
         self.add(layers.BatchNormalization())
         self.add(layers.MaxPool3D(pool_size=(1, 2, 2), strides=None, padding='valid'))
 
         self.add(
-            layers.Conv3D(32, (3, 3, 3), strides=(2, 2, 2), padding='same', use_bias=False,
-                          kernel_initializer='glorot_normal', kernel_regularizer=regularizers.l2))
+            layers.Conv3D(filters=64, kernel_size=(3, 3, 3), padding='same', use_bias=False,
+                          kernel_initializer='glorot_normal'))
         self.add(layers.LeakyReLU(alpha=0.3))
         self.add(layers.BatchNormalization())
         self.add(layers.MaxPool3D(pool_size=(2, 2, 2), strides=None, padding='valid'))
 
         self.add(
-            layers.Conv3D(32, (3, 3, 3), strides=(2, 2, 2), padding='same', use_bias=False,
-                          kernel_initializer='glorot_normal', kernel_regularizer=regularizers.l2))
+            layers.Conv3D(filters=128, kernel_size=(3, 3, 3), padding='same', use_bias=False,
+                          kernel_initializer='glorot_normal'))
         self.add(layers.LeakyReLU(alpha=0.3))
         self.add(layers.BatchNormalization())
         self.add(layers.MaxPool3D(pool_size=(2, 2, 2), strides=None, padding='valid'))
 
         self.add(
-            layers.Conv3D(32, (3, 3, 3), strides=(2, 2, 2), padding='same', use_bias=False,
-                          kernel_initializer='glorot_normal', kernel_regularizer=regularizers.l2))
+            layers.Conv3D(filters=256, kernel_size=(3, 3, 3), padding='same', use_bias=False,
+                          kernel_initializer='glorot_normal'))
         self.add(layers.LeakyReLU(alpha=0.3))
         self.add(layers.BatchNormalization())
         self.add(layers.MaxPool3D(pool_size=(2, 2, 2), strides=None, padding='valid'))
 
         self.add(
-            layers.Conv3D(32, (3, 3, 3), strides=(2, 2, 2), padding='same', use_bias=False,
-                          kernel_initializer='glorot_normal', kernel_regularizer=regularizers.l2))
-        self.add(layers.LeakyReLU(alpha=0.3))
-        self.add(layers.BatchNormalization())
-        self.add(layers.MaxPool3D(pool_size=(2, 2, 2), strides=None, padding='valid'))
+            layers.Conv3D(filters=256, kernel_size=(3, 3, 3), padding='same', use_bias=False,
+                          kernel_initializer='glorot_normal'))
 
-        self.add(layers.Flatten())
-        self.add(layers.Dense(256))
-        self.add(layers.LeakyReLU(alpha=0.3))
-
+        self.add(layers.GlobalAveragePooling3D())
         self.add(layers.Dense(7))
-        self.add(layers.Softmax(-1))
+        self.add(layers.Softmax())
